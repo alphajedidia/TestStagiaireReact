1e671e3f04b4ee00562a25d36fc15272024fffbb
alphajedidia <alphajedidia01@gmail.com>
2023-09-13 11:34:51 +0300

    CreatedReactApp first commit

---
+++ src/redux/userSlice.ts
@@ -0,0 +1,33 @@
+// userSlice.ts
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+
+interface User {
+  id: number;
+  name: string;
+  username:string;
+  email:string;
+  website:string;
+  phone:string;
+  // Add other user properties here
+}
+
+interface UserState {
+  users: User[];
+}
+
+const initialState: UserState = {
+  users: [],
+};
+
+const userSlice = createSlice({
+  name: 'user',
+  initialState,
+  reducers: {
+    setUsers(state, action: PayloadAction<User[]>) {
+      state.users = action.payload;
+    },
+  },
+});
+
+export const { setUsers } = userSlice.actions;
+export default userSlice.reducer;
